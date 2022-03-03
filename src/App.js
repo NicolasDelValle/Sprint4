709093d1f13003da87b2e674d44b3c82fa322b09
Adrian Del Valle <adrianchelo7@gmail.com>
2022-03-03 01:22:32 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import ColContainer from "./components/colContainer";
+import FrutaItem from "./components/item";
 
 function App() {
-  return;
-  <div className="App"></div>;
+  return (
+    <div className="App container">
+      <div className="row">
+        <ColContainer title="Productos disponibles"></ColContainer>
+        <ColContainer title="Carrito de compra"></ColContainer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/item.jsx
@@ -0,0 +1,9 @@
+function FrutaItem(itemIs, id, name, unitPrice) {
+  return (
+    <a href={id} className="list-group-item list-group-item-action">
+      A second link item
+    </a>
+  );
+}
+
+export default FrutaItem;
