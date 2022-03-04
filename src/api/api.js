198ddba369c897cb82ae48e222699750f8809a74
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-04 18:03:27 -0300

    Ejercicio8

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 async function movies() {
   console.log(process.env.API_KEY);
   const response = await axios.get(
-    `https://api.themoviedb.org/3/movie/popular?api_key=f732f580b6c520759bbb69f67243bb95&language=en-US&page=1`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
   );
   console.log(response);
 }
