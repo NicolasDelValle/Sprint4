78f421a6156d61072c848c38145a660af3eb0a4f
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-05 06:21:42 -0300

    ui

--- src/components/bigComponents/navBar.jsx
+++ src/components/bigComponents/navBar.jsx
@@ -2,24 +2,36 @@ export default function PageNavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#">
-          <span className="fs-3" style={{ color: "#DB202C" }}>
-            H
-          </span>
-          ACKFLIX
+        <a
+          id="App-logo"
+          className="navbar-brand border-bottom border-info pb-0 mb-1 lh-1"
+          href="#"
+        >
+          HACKFLIX
         </a>
+        <form className="d-none d-lg-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          ></input>
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler btn btn-lin"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
+          data-bs-target="#responsiveNav"
+          aria-controls="responsiveNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          Explorar
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="responsiveNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
@@ -31,54 +43,24 @@ export default function PageNavBar() {
                 Series
               </a>
             </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider"></hr>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
             <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+              <a className="nav-link" href="#">
+                Peliculas
+              </a>
             </li>
           </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            ></input>
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
+        <form className="d-flex d-lg-none">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          ></input>
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </nav>
   );
