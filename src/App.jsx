ebfb6f6e22a2cc2b090816075d3d2a1b11501989
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-05 05:29:46 -0300

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import "./App.css";
-
-import MovieCard from "./components/moviecard";
-import movies from "./api/movies.json";
+import PageNavBar from "./components/bigComponents/navBar";
+import PageHeader from "./components/bigComponents/header";
 function App() {
-  let cardMovies = [];
-  for (const movie of movies) {
-    cardMovies.push(
-      <MovieCard
-        title={movie.title}
-        year={movie.year}
-        rank={movie.rank}
-        imgUrl={movie.imgUrl}
-      />
-    );
-  }
   return (
-    <div className="App container ">
-      <div className="row row-cols-1 row-cols-md-3 g-5 ">{cardMovies}</div>
+    <div className="App ">
+      <PageNavBar />
+      <PageHeader />
     </div>
   );
 }
