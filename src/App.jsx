6a10054e35fb3bed4ab61236cf2335702b263feb
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-07 08:59:34 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,18 @@ import PageHeader from "./components/bigComponents/header";
 import PageNavBarPhone from "./components/bigComponents/navBarPhone";
 import Api from "./api/api";
 function App() {
-  const handleLog = () => {
-    console.log(Api.getTopRatedMovies());
-  };
+  const mostRatedMovies = Api.getTopRatedMovies;
+  const mostRatedMoviesCards = mostRatedMovies.results.map((movie) => (
+    <p>
+      <h1>{movie.title}</h1>
+    </p>
+  ));
+
   return (
     <div className="App" style={{ backgroundColor: "black" }}>
       <PageNavBar />
 
-      <button onClick={handleLog}>PULSAR</button>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
-        commodi quisquam itaque non culpa delectus animi natus, consequatur cum
-        impedit officiis vel expedita optio facilis assumenda totam voluptatum
-        dignissimos!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
-        commodi quisquam itaque non culpa delectus animi natus, consequatur cum
-        impedit officiis vel expedita optio facilis assumenda totam voluptatum
-        dignissimos!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
-        commodi quisquam itaque non culpa delectus animi natus, consequatur cum
-        impedit officiis vel expedita optio facilis assumenda totam voluptatum
-        dignissimos!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
-        commodi quisquam itaque non culpa delectus animi natus, consequatur cum
-        impedit officiis vel expedita optio facilis assumenda totam voluptatum
-        dignissimos!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
-        commodi quisquam itaque non culpa delectus animi natus, consequatur cum
-        impedit officiis vel expedita optio facilis assumenda totam voluptatum
-        dignissimos!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
-        commodi quisquam itaque non culpa delectus animi natus, consequatur cum
-        impedit officiis vel expedita optio facilis assumenda totam voluptatum
-        dignissimos!
-      </p>
+      {mostRatedMoviesCards}
       <PageNavBarPhone />
     </div>
   );
