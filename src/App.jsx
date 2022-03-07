310f0a2c4eef27f9a067ee69603c0cbbe0adaab6
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-07 01:57:46 -0300

    Requests

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import PageNavBar from "./components/bigComponents/navBar";
 import PageHeader from "./components/bigComponents/header";
 import PageNavBarPhone from "./components/bigComponents/navBarPhone";
+import Api from "./api/api";
 function App() {
+  const handleLog = () => {
+    console.log(Api.getTopRatedMovies());
+  };
   return (
     <div className="App" style={{ backgroundColor: "black" }}>
       <PageNavBar />
-      <PageHeader />
 
+      <button onClick={handleLog}>PULSAR</button>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, possimus
         commodi quisquam itaque non culpa delectus animi natus, consequatur cum
--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,20 @@
 import axios from "axios";
 
-function getMovieList() {}
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+async function getTopRatedMovies() {
+  const movies = await axios(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  console.log("juan ", movies);
+  return movies;
+}
+
+async function getMovieFromSearch(query) {
+  const movies = await axios(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+  );
+  return movies;
+}
+
+export default { getTopRatedMovies, getMovieFromSearch };
