dff0e739fd2e95e3bc63161f1467e9d6e50f9fe0
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-11 21:12:34 -0300

    arreglos

--- src/components/conut.js
+++ src/components/conut.js
@@ -6,34 +6,7 @@ function Count() {
   const dispatch = useDispatch();
   const taskList = useSelector((state) => state);
   const [valueToSet, setValueToSet] = useState(0);
-  return (
-    <div>
-      <Container>
-        <InputGroup className="w-25 mb-3 bg-light">
-          <Button onClick={() => dispatch({ type: "inc" })} variant="success">
-            +
-          </Button>
-          <Button onClick={() => dispatch({ type: "dec" })} variant="danger">
-            -
-          </Button>
-
-          <Button
-            variant="primary"
-            onClick={() =>
-              dispatch({ type: "set", payload: Number(valueToSet) })
-            }
-          >
-            Set
-          </Button>
-          <FormControl
-            onInput={(event) => setValueToSet(event.target.value)}
-            aria-label="Example text with two button addons"
-          />
-          <span>PASOS :D {taskList}</span>
-        </InputGroup>
-      </Container>
-    </div>
-  );
+  return <Container></Container>;
 }
 
 export default Count;
