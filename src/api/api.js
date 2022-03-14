170860bb597b87501260d2657690921a75fc01d0
Nicolas Del Valle <adrianchelo7@gmail.com>
2022-03-14 12:30:55 -0300

    add pages to api

--- src/api/api.js
+++ src/api/api.js
@@ -9,17 +9,17 @@ async function getTrendingMovies(time) {
   return movies;
 }
 
-async function getTopRatedMovies() {
+async function getTopRatedMovies(page) {
   const movies = await axios(
-    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=es-ES&page=1`
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=es-ES&page=${page}`
   );
 
   return movies;
 }
 
-async function getMovieFromSearch(query) {
+async function getMovieFromSearch(query, page) {
   const movies = await axios(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${query}&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=es-ES&query=${query}&page=${page}&include_adult=false`
   );
   return movies;
 }
@@ -39,9 +39,9 @@ async function getGenres() {
   return generes;
 }
 
-async function getMoviesWhitFilters(rate, year, genre) {
+async function getMoviesWhitFilters(rate, year, genre, page) {
   const movies =
-    await axios(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&year=${year}&vote_count.gte=50&vote_average.lte=${rate}&with_genres=${genre}&with_watch_monetization_types=flatrate
+    await axios(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${page}&year=${year}&vote_count.gte=50&vote_average.lte=${rate}&with_genres=${genre}&with_watch_monetization_types=flatrate
 `);
   return movies;
 }
